db645180864a1d6604547e71ba525b5ac198e359
Pablo Apaoblaza <pablo.apaoblaza@gmail.com>
2024-12-13 22:29:44 -0300

    corrige errores de input

--- conversor.js
+++ conversor.js
@@ -41,7 +41,7 @@ renderDivisas(); //funcion de dibujar el
 
 buscarbutton.addEventListener("click", function () {
   let resultadoText = "Resultado: ";
-  if (!isNaN(clp.value)) {
+  if (!isNaN(clp.value) && select.selectedIndex !== 0) {
     resultadoText += clp.value / select.value;
     resultadoText += `<br><p>Tipo de Cambio: `;
     resultadoText += select.value;
@@ -50,34 +50,44 @@ buscarbutton.addEventListener("click", function () {
     const optionselected = select.options[selectIndex];
     renderGrafica(optionselected.id);
   } else {
-    alert("por favor ingresar un numero");
+    alert("por favor ingresar un numero o seleccionar una moneda");
   }
 });
 
 async function getDataDivisas(selectid) {
-  let link = "https://mindicador.cl/api/";
-  link += selectid;
-  link += "/2024";
-  const res = await fetch(link);
-  const datos = await res.json();
-  const labels = datos.serie.map((item) => item.fecha.slice(0, 10)).reverse();
-  const valores = datos.serie.map((item) => Number(item.valor)).reverse();
-  const datasets = [
-    {
-      label: "Tipo de Cambio durante 2024",
-      borderColor: "red",
-      data: valores,
-    },
-  ];
-  return { labels, datasets }; //retorna "data"
+  try {
+    let link = "https://mindicador.cl/api/";
+    link += selectid;
+    link += "/2024";
+    const res = await fetch(link);
+    const datos = await res.json();
+    const labels = datos.serie.map((item) => item.fecha.slice(0, 10)).reverse();
+    const valores = datos.serie.map((item) => Number(item.valor)).reverse();
+    const datasets = [
+      {
+        label: "Tipo de Cambio durante 2024",
+        borderColor: "red",
+        data: valores,
+      },
+    ];
+    return { labels, datasets }; //retorna "data"
+  } catch (error) {
+    resultado.innerHTML =
+      "Ha ocurrido un error inesperado. Por favor intenta mas tarde";
+  }
 }
 
 async function renderGrafica(selectid) {
-  const data = await getDataDivisas(selectid);
-  const config = { type: "line", data };
-  myChart.style.backgroundColor = "white";
-  if (chartInstance !== null) {
-    chartInstance.destroy();
+  try {
+    const data = await getDataDivisas(selectid);
+    const config = { type: "line", data };
+    myChart.style.backgroundColor = "white";
+    if (chartInstance !== null) {
+      chartInstance.destroy();
+    }
+    chartInstance = new Chart(myChart, config);
+  } catch (error) {
+    resultado.innerHTML =
+      "Ha ocurrido un error inesperado. Por favor intenta mas tarde";
   }
-  chartInstance = new Chart(myChart, config);
 }
